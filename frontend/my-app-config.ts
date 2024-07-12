4eecc84da8acffab4580c7b17765bc8852a17263
ZIBO <wangzibo@bu.edu>
2024-07-12 19:16:43 -0400

    Add files via upload

---
+++ frontend/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaglx8eghCUYyI5s5d7',
+        issuer: 'https://dev-85101865.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
